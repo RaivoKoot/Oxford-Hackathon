521de9c66045ae2133fb6ed2d8667c0c1a063248
Raivo Koot <raivokoot@gmail.com>
2018-11-25 03:50:36 +0000

    python prediction request script now takes the url as a script argument

--- model_request_prediction/RunScript.java
+++ model_request_prediction/RunScript.java
@@ -1,9 +1,10 @@
 
 public class RunScript {
 
-  public static void runPythonScript(String scriptName){
+  public static void runPythonScript(String scriptName, String arguments){
     try{
-      Process p = Runtime.getRuntime().exec("python " + scriptName);
+
+      Process p = Runtime.getRuntime().exec("python " + scriptName +" "+arguments);
     } catch(Exception e){
       System.out.println(e);
     }
--- model_request_prediction/get_prediction.py
+++ model_request_prediction/get_prediction.py
@@ -5,8 +5,8 @@ project_id = '40412ce6-fe45-4fc2-b949-9bb6fe28abf2'
 predictor = CustomVisionPredictionClient('57c2350f261b4cf0aec943bcf893e2d8', endpoint=ENDPOINT)
 
 import sys
-#img_url = sys.argv[1]
-img_url = 'https://images.pexels.com/photos/259962/pexels-photo-259962.jpeg?auto=compress&cs=tinysrgb&h=350'
+img_url = sys.argv[1]
+#img_url = 'https://images.pexels.com/photos/259962/pexels-photo-259962.jpeg?auto=compress&cs=tinysrgb&h=350'
 
 results =  predictor.predict_image_url(project_id, url=img_url)
 
